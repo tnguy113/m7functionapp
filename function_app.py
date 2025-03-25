baed7b9f4639d0952b87f6de2cf251ece129339d
Ton That Tu Nguyen <tonthattuit@gmail.com>
2025-03-24 23:41:04 -0400

    Added files

---
+++ function_app.py
@@ -0,0 +1,39 @@
+import azure.functions as func
+import logging
+import uuid
+from azure.storage.blob import BlobServiceClient
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="http_m7name_trigger")
+def http_m7name_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        file_name = f'name-{str(uuid.uuid4()).split("-")[0]}'
+        write_to_blob_storage(file_name, name)
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+def write_to_blob_storage(file_name, content):
+    try:
+        blobServiceClient = BlobServiceClient.from_connection_string('DefaultEndpointsProtocol=https;AccountName=m7functionapprg9b35;AccountKey=y/sCqYrU40YZS6czxWX9PEeOQYVB5NA4uiDtkwsCSDmhrTZs/HdCkJ0Q/6OmPJkpLCMahHeZpUvB+ASt6CQMyQ==;EndpointSuffix=core.windows.net')
+        containerClient = blobServiceClient.get_container_client('output-data')
+        blobClient = containerClient.get_blob_client(blob=file_name)
+        blobClient.upload_blob(content, overwirte=True)
+        logging.info(f'Create new file {file_name} successfully.')
+    except Exception as e:
+        logging.error(f'An exception is found: {e}')
