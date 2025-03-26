ae701da80e3c8e8310999c5bfbe6de98b90e7e7a
Ton That Tu Nguyen <tnguy113@vols.utk.edu>
2025-03-25 23:22:25 -0400

    Update comment to explain the code

--- function_app.py
+++ function_app.py
@@ -5,26 +5,46 @@ from azure.storage.blob import BlobServiceClient
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
+
 @app.route(route="http_m7name_trigger", auth_level=func.AuthLevel.ANONYMOUS)
 def http_m7name_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    
+    """
+    HTTP trigger function that responds to a GET request with a personalized message.
+
+    This function processes an HTTP request, extracts the 'name' query parameter,
+    and returns a personalized greeting. If the 'name' parameter is provided, it
+    is used to create a text file with value of 'name' is the content and then
+    upload to Azure Blob Storage.
+    If the 'name' parameter is not provided, a default message is returned.
+
+    Parameters:
+    req (func.HttpRequest): The HTTP request object.
+
+    Returns:
+    func.HttpResponse: A response object containing the greeting message.
+    """
+
+    logging.info("Python HTTP trigger function processed a request.")
+
     # Parse query parameter name
-    name = req.params.get('name')
-    logging.info(f'Param name = {name}.')
-    
+    name = req.params.get("name")
+    logging.info(f"Param name = {name}.")
+
     # If name is provided in the query string, use it as the file content, build the file name and upload to blob container
     if name:
         file_name = f'name-{str(uuid.uuid4()).split("-")[0]}.txt'
         write_to_blob_storage(file_name, name)
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    
+        return func.HttpResponse(
+            f"Hello, {name}. This HTTP triggered function executed successfully."
+        )
+
     # If name is not provided in the query string, send the response with a default message with status code 200
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200,
         )
+
 
 def write_to_blob_storage(file_name, content):
     """
@@ -38,10 +58,12 @@ def write_to_blob_storage(file_name, content):
     None
     """
     try:
-        blobServiceClient = BlobServiceClient.from_connection_string('DefaultEndpointsProtocol=https;AccountName=blobstrgm5;AccountKey=qmbk2otAIGUo+URZYZTbFtD6ZbVWp5+5vsYv9bWDTlbIsFNMHYFHBedagCQexDG4xvX+bHE+MaJg+AStjmiAJw==;EndpointSuffix=core.windows.net')
-        containerClient = blobServiceClient.get_container_client('output-data')
+        blobServiceClient = BlobServiceClient.from_connection_string(
+            "DefaultEndpointsProtocol=https;AccountName=blobstrgm5;AccountKey=qmbk2otAIGUo+URZYZTbFtD6ZbVWp5+5vsYv9bWDTlbIsFNMHYFHBedagCQexDG4xvX+bHE+MaJg+AStjmiAJw==;EndpointSuffix=core.windows.net"
+        )
+        containerClient = blobServiceClient.get_container_client("output-data")
         blobClient = containerClient.get_blob_client(blob=file_name)
         blobClient.upload_blob(content)
         logging.info(f'Created new file "{file_name}.txt" successfully.')
     except Exception as e:
-        logging.error(f'An exception is found: {e}')
+        logging.error(f"An exception is found: {e}")
