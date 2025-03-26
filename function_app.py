7363b227f86df2ed256cd1ea8884dc5bbacdbfb3
Ton That Tu Nguyen <tnguy113@vols.utk.edu>
2025-03-25 23:23:37 -0400

    Update route name

--- function_app.py
+++ function_app.py
@@ -6,7 +6,7 @@ from azure.storage.blob import BlobServiceClient
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
-@app.route(route="http_m7name_trigger", auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(route="http_to_blob_trigger", auth_level=func.AuthLevel.ANONYMOUS)
 def http_m7name_trigger(req: func.HttpRequest) -> func.HttpResponse:
     """
     HTTP trigger function that responds to a GET request with a personalized message.
